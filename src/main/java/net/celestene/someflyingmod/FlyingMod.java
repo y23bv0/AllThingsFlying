048add325c89b31e0ea2de3537d7a9cc4d5638ed
y23bv0 <dakhmedov50@stuy.edu>
2026-05-13 18:02:46 -0400

    Implented items with textures into the game!

--- src/main/java/net/celestene/someflyingmod/FlyingMod.java
+++ src/main/java/net/celestene/someflyingmod/FlyingMod.java
@@ -1,6 +1,9 @@
 package net.celestene.someflyingmod;
 
 import com.mojang.logging.LogUtils;
+import net.celestene.someflyingmod.item.ModCreativeModTabs;
+import net.celestene.someflyingmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -25,6 +28,10 @@ public class FlyingMod
     public FlyingMod(FMLJavaModLoadingContext context) {
         IEventBus modEventBus = context.getModEventBus();
 
+        ModCreativeModTabs.register(modEventBus);
+
+        ModItems.register(modEventBus);
+
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
         // Register ourselves for server and other game events we are interested in
@@ -41,7 +48,10 @@ public class FlyingMod
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.SAPPHIRE);
+            event.accept(ModItems.KITE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/net/celestene/someflyingmod/item/ModItems.java
@@ -0,0 +1,24 @@
+package net.celestene.someflyingmod.item;
+
+import net.celestene.someflyingmod.FlyingMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, FlyingMod.MODID);
+
+    public static final RegistryObject<Item> SAPPHIRE = ITEMS.register("sapphire",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> KITE = ITEMS.register("kite",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+
+    }
+}
