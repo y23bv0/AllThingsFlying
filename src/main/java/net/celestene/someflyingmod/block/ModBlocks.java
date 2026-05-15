86ac16bc6c6bf0edbce542b89ec7fa6e2ebcc218
y23bv0 <dakhmedov50@stuy.edu>
2026-05-14 23:51:41 -0400

    Added mining properties to blocks

--- src/main/java/net/celestene/someflyingmod/block/ModBlocks.java
+++ src/main/java/net/celestene/someflyingmod/block/ModBlocks.java
@@ -2,10 +2,12 @@ package net.celestene.someflyingmod.block;
 
 import net.celestene.someflyingmod.FlyingMod;
 import net.celestene.someflyingmod.item.ModItems;
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.DropExperienceBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -20,10 +22,18 @@ public class ModBlocks {
             DeferredRegister.create(ForgeRegistries.BLOCKS, FlyingMod.MODID);
 
     public static final RegistryObject<Block> FLYING_ESSENCE = registerBlock("flying_essence",
-            () -> new Block(BlockBehaviour.Properties.copy(Blocks.OBSIDIAN).sound(SoundType.AMETHYST)));
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.DIORITE).sound(SoundType.AMETHYST)));
 
     public static final RegistryObject<Block> FLIGHTLESS_POWDER = registerBlock("flightless_powder",
             () -> new Block(BlockBehaviour.Properties.copy(Blocks.GRAVEL).sound(SoundType.SAND)));
+
+    public static final RegistryObject<Block> ESSENCE_ORE = registerBlock("essence_ore",
+            () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.STONE).strength(2f)
+                    .requiresCorrectToolForDrops(), UniformInt.of(3, 6)));
+    // requiresCorrectToolForDrops is inherited from stone regardless, keeping it here as a reminder!
+    // UniformInt.of specifies the range of experience orbs when mining that block!
+
+    // finish the rest of this block tmrw!
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
