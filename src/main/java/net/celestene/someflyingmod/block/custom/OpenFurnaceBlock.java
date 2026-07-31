c7b6f6bada68a55e564f53d55ea068254637757d
Spagles <106791090+Spagles@users.noreply.github.com>
2026-07-31 09:18:16 +0000

    Refactor OpenFurnaceBlock to extend AbstractFurnaceBlock and implement container opening logic

--- src/main/java/net/celestene/someflyingmod/block/custom/OpenFurnaceBlock.java
+++ src/main/java/net/celestene/someflyingmod/block/custom/OpenFurnaceBlock.java
@@ -1,18 +1,33 @@
 package net.celestene.someflyingmod.block.custom;
 
+import net.celestene.someflyingmod.block.entity.ModBlockEntities;
 import net.celestene.someflyingmod.block.entity.OpenFurnaceBlockEntity;
 import net.minecraft.core.BlockPos;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.util.RandomSource;
-import net.minecraft.world.level.block.FurnaceBlock;
+import net.minecraft.stats.Stats;
+import net.minecraft.world.MenuProvider;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.AbstractFurnaceBlock;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.Nullable;
 
-public class OpenFurnaceBlock extends FurnaceBlock {
+public class OpenFurnaceBlock extends AbstractFurnaceBlock {
 
     public OpenFurnaceBlock(Properties pProperties) {
         super(pProperties);
+    }
+
+    @Override
+    protected void openContainer(Level pLevel, BlockPos pPos, Player pPlayer) {
+        BlockEntity blockentity = pLevel.getBlockEntity(pPos);
+        if (blockentity instanceof OpenFurnaceBlockEntity) {
+            pPlayer.openMenu((MenuProvider)blockentity);
+            pPlayer.awardStat(Stats.INTERACT_WITH_FURNACE);
+        }
     }
 
     @Override
@@ -26,7 +41,7 @@ public class OpenFurnaceBlock extends FurnaceBlock {
     }
 
     @Override
-    public void tick(BlockState pState, ServerLevel pLevel, BlockPos pPos, RandomSource pRandom) {
-        super.tick(pState, pLevel, pPos, pRandom);
+    public <T extends BlockEntity> @Nullable BlockEntityTicker<T> getTicker(Level pLevel, BlockState pState, BlockEntityType<T> pBlockEntityType) {
+        return createFurnaceTicker(pLevel, pBlockEntityType, ModBlockEntities.OPEN_FURANCE_BE.get());
     }
 }
