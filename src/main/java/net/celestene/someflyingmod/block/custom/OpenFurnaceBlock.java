ab8f1a8ffc4a58c0f2fca94a10000782777ecd8c
y23bv0 <dakhmedov50@stuy.edu>
2026-08-05 14:20:40 -0400

    Added/Fixed Open Furnace Functionality: - Smelts items like a normal furnace - You can right click the top of the furnace with clay to warm it. After 10 seconds, it will become Dry Clay. You can right click the top and dry clay will pop out - The furnace can simulatenously do both of these actions
    Fixed Open Furnace texturing issues when on
    
    To be implemented:
    - Rendering for the item placed on top of the furnace

--- src/main/java/net/celestene/someflyingmod/block/custom/OpenFurnaceBlock.java
+++ src/main/java/net/celestene/someflyingmod/block/custom/OpenFurnaceBlock.java
@@ -2,20 +2,40 @@ package net.celestene.someflyingmod.block.custom;
 
 import net.celestene.someflyingmod.block.entity.ModBlockEntities;
 import net.celestene.someflyingmod.block.entity.OpenFurnaceBlockEntity;
+import net.celestene.someflyingmod.item.ModItems;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.stats.Stats;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.MenuProvider;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.AbstractFurnaceBlock;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.RenderShape;
+import net.minecraft.world.level.block.entity.AbstractFurnaceBlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.Vec3;
+import net.minecraftforge.items.ItemStackHandler;
+import org.checkerframework.checker.units.qual.A;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.List;
+
+import static net.celestene.someflyingmod.block.entity.OpenFurnaceBlockEntity.clickedTop;
+
 public class OpenFurnaceBlock extends AbstractFurnaceBlock {
+
 
     public OpenFurnaceBlock(Properties pProperties) {
         super(pProperties);
@@ -31,6 +51,22 @@ public class OpenFurnaceBlock extends AbstractFurnaceBlock {
     }
 
     @Override
+    public InteractionResult use(BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand, BlockHitResult pHit) {
+        if(clickedTop(pPos, pHit)){
+            BlockEntity be = pLevel.getBlockEntity(pPos);
+            if(!pLevel.isClientSide()){
+                if (be instanceof OpenFurnaceBlockEntity) {
+                    ((OpenFurnaceBlockEntity) be).handleTopClick(pLevel, pPos, pPlayer, (OpenFurnaceBlockEntity) be);
+                }
+            }
+            return InteractionResult.sidedSuccess(pLevel.isClientSide);
+        }
+        return super.use(pState, pLevel, pPos, pPlayer, pHand, pHit);
+    }
+
+
+
+    @Override
     public RenderShape getRenderShape(BlockState pState) {
         return RenderShape.MODEL;
     }
@@ -42,6 +78,21 @@ public class OpenFurnaceBlock extends AbstractFurnaceBlock {
 
     @Override
     public <T extends BlockEntity> @Nullable BlockEntityTicker<T> getTicker(Level pLevel, BlockState pState, BlockEntityType<T> pBlockEntityType) {
-        return createFurnaceTicker(pLevel, pBlockEntityType, ModBlockEntities.OPEN_FURANCE_BE.get());
+
+        if(!pLevel.isClientSide){
+            if(pBlockEntityType == ModBlockEntities.OPEN_FURANCE_BE.get()){
+                return (BlockEntityTicker<T>) (level, pos, state, blockEntity) -> OpenFurnaceBlockEntity.myServerTick(level, pos, state, (OpenFurnaceBlockEntity) blockEntity);
+            } else {
+                return null;
+            }
+        } else {
+            return null;
+        }
+
+//        return createFurnaceTicker(pLevel, pBlockEntityType, ModBlockEntities.OPEN_FURANCE_BE.get());
+//        return pBlockEntityType == ModBlockEntities.OPEN_FURANCE_BE.get() ? OpenFurnaceBlockEntity::tick : null;
     }
+
+
 }
+
--- src/main/java/net/celestene/someflyingmod/block/entity/OpenFurnaceBlockEntity.java
+++ src/main/java/net/celestene/someflyingmod/block/entity/OpenFurnaceBlockEntity.java
@@ -1,19 +1,90 @@
 package net.celestene.someflyingmod.block.entity;
 
+import net.celestene.someflyingmod.item.ModItems;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.MenuProvider;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.FurnaceMenu;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.RecipeType;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.AbstractFurnaceBlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.Vec3;
+
+import java.util.UUID;
 
 public class OpenFurnaceBlockEntity extends AbstractFurnaceBlockEntity implements MenuProvider {
+    private ItemStack topItem = ItemStack.EMPTY;
+    private int COOK_PROGRESS = 0;
+    private final int COOK_TIME = 200;
 
     public OpenFurnaceBlockEntity(BlockPos pPos, BlockState pBlockState) {
         super(ModBlockEntities.OPEN_FURANCE_BE.get(), pPos, pBlockState, RecipeType.SMELTING);
+    }
+
+
+    public static void myServerTick(Level pLevel, BlockPos pPos, BlockState pState, OpenFurnaceBlockEntity pBlockEntity) {
+        AbstractFurnaceBlockEntity.serverTick(pLevel, pPos, pState, pBlockEntity);
+
+        if (!pLevel.isClientSide() && !pBlockEntity.topItem.isEmpty()) {
+
+            if (pBlockEntity.COOK_PROGRESS < pBlockEntity.COOK_TIME) {
+                // Cookable items:
+                if (pBlockEntity.topItem.is(Items.CLAY_BALL)) {
+                    pBlockEntity.COOK_PROGRESS += 20;
+                }
+            } else {
+                if (pBlockEntity.topItem.is(Items.CLAY_BALL)) {
+                    pBlockEntity.topItem = new ItemStack(ModItems.DRY_CLAY.get());
+                    pBlockEntity.COOK_PROGRESS = 0;
+                }
+            }
+
+        }
+    }
+
+    public void handleTopClick(Level pLevel, BlockPos pPos, Player pPlayer, OpenFurnaceBlockEntity pBlockEntity){
+        if (topItem.isEmpty() && !pPlayer.getItemInHand(InteractionHand.MAIN_HAND).isEmpty()) {
+
+            ItemStack playerStack = pPlayer.getItemInHand(InteractionHand.MAIN_HAND);
+            topItem = pPlayer.getMainHandItem().copyWithCount(1);
+            if (!pPlayer.getAbilities().instabuild) {
+                playerStack.shrink(1);
+            }
+            COOK_PROGRESS = 0;
+        } else if (!topItem.isEmpty()){
+
+            double posX = pPos.getX() + 0.5;
+            double posY = pPos.getY() + 1.125;
+            double posZ = pPos.getZ() + 0.5;
+
+            ItemEntity topItem_dropped = new ItemEntity(pLevel, posX, posY, posZ, topItem);
+            pLevel.addFreshEntity(topItem_dropped);
+            pLevel.playSound(null, posX, posY, posZ, SoundEvents.ITEM_PICKUP, SoundSource.PLAYERS, 1.0F, 1.0F);
+            topItem = ItemStack.EMPTY;
+            COOK_PROGRESS = 0;
+
+        }
+    }
+
+    public static boolean clickedTop(BlockPos pPos, BlockHitResult pHit){
+        Vec3 hitVec = pHit.getLocation();
+        int x = (int)((hitVec.x - pPos.getX()) * 15);
+        int y = (int)((hitVec.y - pPos.getY()) * 15);
+        int z = (int)((hitVec.z - pPos.getZ()) * 15);
+
+        return ((3 <= x) && (x <= 12) && (3 <= z) && (z <= 12) && (y == 15));
     }
 
     protected Component getDefaultName() {
@@ -23,4 +94,5 @@ public class OpenFurnaceBlockEntity extends AbstractFurnaceBlockEntity implement
     protected AbstractContainerMenu createMenu(int pId, Inventory pPlayer) {
         return new FurnaceMenu(pId, pPlayer, this, this.dataAccess);
     }
+
 }
